57b6c1e8185d97d2c52e79d96f2958f094317e48
Loong <admin@mc-m.net>
2023-09-28 21:06:39 +0800

    Del windows file and update say.js

--- say.js
+++ say.js
@@ -1,34 +1,9 @@
-const { exec } = require('child_process');
-
 function startGame(){
-    var startFlagFileName = "startFlag.txt";
-    var fs = require("fs");
-    fs.exists(startFlagFileName, function(exists) {
-        if(!exists){
-            var startGameFileName = prompt("请输入您的第三方启动器目录(如有参数请附加)：");
-            fs.writeFile(startFlagFileName, startGameFileName, function(err){})
-        }
-        var starterName;
-        fs.readFile(startFlagFileName,function(err, data){
-            starterName = data.toString();
-        })
-        exec(starterName,(err, stdout, stderr) => {
-            if(err){
-                alert('Crash on 15 lines: Cannot run command.')
-                return -1;
-            }
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
-        })
-    });
+    alert("还不支持手机端啦~QwQ");
 }
 
 function res(){
-    var win = gui.Window.open('https://remcbox.flarum.cloud', {
-        position: 'center',
-        width: 901,
-        height: 127
-    });
+    window.open("https://remcbox.flarum.cloud");
 }
 
 function signup(){res();}
