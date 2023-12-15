00df6b24dfe5bd87c61479881c3293d650352f0f
Jankie <notflyloong@outlook.com>
2023-12-15 21:46:27 +0800

    Remove i18n

--- macos.py
+++ macos.py
@@ -14,11 +14,11 @@ class Application(tk.Tk):
         self.label = tk.Label(self, text="多玩我的世界盒子")
         self.label.pack(anchor="nw", padx=10, pady=10)
 
-        self.button1 = tk.Button(self, text="资源中心 Download Map")
+        self.button1 = tk.Button(self, text="资源中心")
         self.button1.config(width=25, bg="#ABCDEF", bd=0, anchor="w")
         self.button1.pack(side="top", padx=10, pady=10)
 
-        self.button2 = tk.Button(self, text="论坛 BBS")
+        self.button2 = tk.Button(self, text="论坛")
         self.button2.config(width=25, bg="#ABCDEF", bd=0, anchor="w")
         self.button2.pack(side="top", padx=10, pady=10)
 
