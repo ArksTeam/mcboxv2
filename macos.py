9f441239e52ddd755193e5d26a0cd1f18f4f09e1
Jankie <notflyloong@outlook.com>
2023-12-15 21:43:41 +0800

    Update macos ui

--- macos.py
+++ macos.py
@@ -9,16 +9,21 @@ class Application(tk.Tk):
         self.title("多玩我的世界盒子")
         self.geometry("500x200")
 
+        self.configure(bg="#eef2f6")  # 设置应用程序的背景色
+
         self.label = tk.Label(self, text="多玩我的世界盒子")
         self.label.pack(pady=50)
 
-        self.button = tk.Button(self, text="资源中心 Download Map", command=self.on_button_click)
-        self.button.pack()
-        self.button = tk.Button(self, text="论坛 BBS", command=self.on_button_click)
-        self.button.pack()
+        self.button1 = tk.Button(self, text="资源中心 Download Map", command=self.on_button1_click)
+        self.button1.pack()
+        self.button2 = tk.Button(self, text="论坛 BBS", command=self.on_button2_click)
+        self.button2.pack()
 
-    def on_button_click(self):
+    def on_button1_click(self):
         webbrowser.open("https://remcbox.flarum.cloud")
+
+    def on_button2_click(self):
+        webbrowser.open("https://bbs.example.com")
 
 def main() -> int:
     app = Application()
