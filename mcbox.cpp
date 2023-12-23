bf001c322086d9626a802f84ddeb39f73ea69ea2
Jankie <notflyloong@outlook.com>
2023-12-23 16:28:38 +0800

    Update

--- mcbox.cpp
+++ mcbox.cpp
@@ -1,22 +1,22 @@
-#include <fstream>
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    const std::string nwjs_binfile = "nwjs.exe";
-    int code = system(nwjs_binfile.c_str());
-    if(code != 0){
-        std::cerr << "程序崩溃了，请查看logs.log文件并反馈Bug。" << std::endl;
-        try{
-            std::ofstream outfile("logs.log");
-            outfile << std::to_string(code) << std::endl;
-            outfile.close();
-        }catch(...){
-            std::cerr << "程序崩溃了，请反馈Bug并允许开发人员远程控制您的电脑检查问题。" << std::endl;
-        }
-        std::cerr << "按Ctrl+C关闭错误窗口。" << std::endl;
-        while (true){
+const char *nwjs_filename = "nw.exe 2>nul";
 
-        }
+int main()
+{
+    int code = system(nwjs_filename);
+    if (code == 0){
+        return 0;
+    }
+    for(int i=0;i<30;i++){
+        printf("*");
+    }
+    printf("\n\n---CRASH---\n程序崩溃了，请反馈Bug并允许开发人员远程控制您的电脑检查问题。\n");
+    printf("The program crashes, please report the Bug and allow the developer to remotely control your computer to check the problem.\n");
+    printf("Code: %d\n", code);
+    while(1){
     }
     return 0;
 }
