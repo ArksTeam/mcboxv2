223cc2eb88fb713dd1b58de5d1d6a043b9ec947f
Jankie <notflyloong@outlook.com>
2023-12-23 16:37:11 +0800

    Update starter

--- mcbox.cpp
+++ mcbox.cpp
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
 
 const char *nwjs_filename = "nw.exe 2>nul";
 
 int main()
 {
+    HWND hwnd;
+	hwnd=FindWindow("ConsoleWindowClass",NULL);
+	if(hwnd)
+	{
+		ShowWindow(hwnd,SW_HIDE);
+	}
     int code = system(nwjs_filename);
     if (code == 0){
         return 0;
     }
-    for(int i=0;i<30;i++){
-        printf("*");
-    }
-    printf("\n\n---CRASH---\n程序崩溃了，请反馈Bug并允许开发人员远程控制您的电脑检查问题。\n");
-    printf("The program crashes, please report the Bug and allow the developer to remotely control your computer to check the problem.\n");
-    printf("Code: %d\n", code);
-    while(1){
-    }
-    return 0;
+    MessageBox(NULL,"The program crashes, please report the Bug and allow the developer to remotely control your computer to check the problem.\n","CRASH",NULL);
+    return code;
 }
