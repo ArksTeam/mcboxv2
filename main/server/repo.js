2078a542deceac21544280a0c3190ed9726a88d5
Jankie <qwq@awaish.link>
2024-01-28 18:29:37 +0800

    Update link

--- main/server/repo.js
+++ main/server/repo.js
@@ -21,8 +21,33 @@ function startGame(){
     });
 }
 
+class LCG {
+    constructor(seed, a, c, m) {
+        this.seed = seed;
+        this.a = a;
+        this.c = c;
+        this.m = m;
+    }
+
+    next01() {
+      this.seed = (this.a * this.seed + this.c) % this.m;
+      return this.seed / this.m; // 将生成的数映射到[0, 1)区间上
+    }
+    nextInRange(min, max) {
+    const range = max - min;
+      return min + range * this.next01(); // 将[0, 1)区间上的数映射到[min, max)区间上
+    }
+}
+
 function res(){
-    window.open("https://bbs.minecraftbox.link/");
+    const lcg = new LCG(42, 1664525, 1013904223, Math.pow(2, 32));
+    const range = lcg.nextInRange(1, 2);
+    if(range<0.5){
+        window.open('https://bbs.minecraftbox.link');
+    }else{
+        window.open('https://flarum.minecraftbox.link')
+    }
+
 }
 
 function signup(){res();}
